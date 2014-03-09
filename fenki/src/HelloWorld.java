c5c2b16fbe2b53a9d4054fffba6d77b0f23c546e
Anya Kaygorodova <anutakay@gmail.com>
2014-03-09 16:09:15 +0800

    Maked Look for Mac OS X.

--- fenki/src/HelloWorld.java
+++ fenki/src/HelloWorld.java
@@ -1,3 +1,4 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 import ru.fenki.panels.MyTabbedPane;
@@ -7,6 +8,11 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class HelloWorld extends JFrame {
 
@@ -15,39 +21,77 @@ public class HelloWorld extends JFrame {
 	 */
 	private static final long serialVersionUID = 3431369082324981806L;
 	JTabbedPane tabbedPane;
+	boolean IS_MAC;
 
 	public HelloWorld(String string) {
-		// TODO Auto-generated constructor stub
 		super(string);
+	}
+	
+	public static void main(String[] args) {
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				HelloWorld frame = new HelloWorld("Феньки");
+				frame.createGUI();
+			}
+		});
 	}
 
 	public void createGUI() {
 
-		Font font = new Font("Verdana", Font.PLAIN, 11);
+		String lcOSName = System.getProperty("os.name").toLowerCase();
+		IS_MAC = lcOSName.startsWith("mac os x");
+		System.setProperty("apple.laf.useScreenMenuBar", "true");
+		System.setProperty("com.apple.mrj.application.apple.menu.about.name", "WikiTeX");
+		
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedLookAndFeelException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.setProperty("com.apple.macos.smallTabs", "true");
 
-		// JFrame frame = new JFrame("Феньки");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		tabbedPane = new JTabbedPane();
-
 		setContentPane(tabbedPane);
-
 		tabbedPane.addTab("", new MyTabbedPane());
-		tabbedPane.setTabComponentAt(tabbedPane.getTabCount() - 1, tab(""));
+		tabbedPane.setTabComponentAt(tabbedPane.getTabCount() - 1, tab("Безымянный"));
 
+		setJMenuBar(createMenuBar());
+
+		setPreferredSize(new Dimension(600, 600));
+		pack();
+		setLocationRelativeTo(null);
+		setVisible(true);
+	}
+	
+	private JMenuBar createMenuBar() {
 		JMenuBar menu = new JMenuBar();
+		Font font = new Font("Verdana", Font.PLAIN, 11);
+		String bStr="";
+		String eStr="";
+		
+		if(!IS_MAC){
+			bStr="<html><body leftmargin=15 marginwidth=15>";
+			eStr="</body></html>";
+		}
 
-		JMenu fileMenu = new JMenu("<html><body leftmargin=15 marginwidth=15>"
-				+ "Файл" + "</body></html>");
+		JMenu fileMenu = new JMenu(bStr + "Файл" + eStr);
 		fileMenu.setFont(font);
 
-		JMenuItem createItem = new JMenuItem(
-				"<html><body leftmargin=15 marginwidth=15>" + "Создать"
-						+ "</body></html>");
+		JMenuItem createItem = new JMenuItem(bStr + "Создать"+ eStr);
 		createItem.setFont(font);
-		JMenuItem openItem = new JMenuItem(
-				"<html><body leftmargin=15 marginwidth=15>" + "Открыть"
-						+ "</body></html>");
+		JMenuItem openItem = new JMenuItem(bStr + "Открыть"+ eStr);
 		openItem.setFont(font);
 		openItem.addActionListener(new ActionListener() {
 
@@ -57,16 +101,14 @@ public class HelloWorld extends JFrame {
 				JFileChooser openFile = new JFileChooser("./files");
 				int ret = openFile.showDialog(null, "Открыть файл");
 				if (ret == JFileChooser.APPROVE_OPTION) {
-					//File file = openFile.getSelectedFile();
-					// tabbedPane.addTab("", new MyTabbedPane( ));
-					// tabbedPane.setTabComponentAt(tabbedPane.getTabCount()-1,
-					// tab(file.getName()));
+					File file = openFile.getSelectedFile();
+					 tabbedPane.addTab("", new MyTabbedPane( ));
+					 tabbedPane.setTabComponentAt(tabbedPane.getTabCount()-1,
+					 tab(file.getName()));
 				}
 			}
 		});
-		JMenuItem saveItem = new JMenuItem(
-				"<html><body leftmargin=15 marginwidth=15>" + "Сохранить"
-						+ "</body></html>");
+		JMenuItem saveItem = new JMenuItem(bStr + "Сохранить"+ eStr);
 		saveItem.setFont(font);
 
 		fileMenu.add(createItem);
@@ -74,37 +116,22 @@ public class HelloWorld extends JFrame {
 		fileMenu.add(saveItem);
 
 		menu.add(fileMenu);
-		setJMenuBar(menu);
-
-		setPreferredSize(new Dimension(600, 600));
-		pack();
-		setLocationRelativeTo(null);
-		setVisible(true);
+		return menu;
 	}
-
-	public static void main(String[] args) {
-
-		javax.swing.SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-				HelloWorld frame = new HelloWorld("Феньки");
-				frame.createGUI();
-
-			}
-		});
-	}
-
+	
 	private JComponent tab(String str) {
 		JComponent panel = new JPanel(new GridBagLayout());
 		panel.setOpaque(false);
-		// = new
-		// JLabel("<html><body leftmargin=15 topmargin=8 marginwidth=15 marginheight=5>"
-		// + str +"</body></html>");
 		JLabel lblTitle = new JLabel(
 				"<html><body leftmargin=15 topmargin=8 marginwidth=15 marginheight=5>"
 						+ str + "</body></html>");
-		JLabel btnClose = new JLabel(
-				"<html><body rightmargin=5 leftmargin=5 topmargin=8 marginwidth=5 marginheight=5>"
-						+ "x" + "</body></html>");
+		ImageIcon icon = new ImageIcon("image/close.png");
+		if(icon.getImage()==null){
+			System.out.print("not found");
+		}
+
+		JLabel btnClose = new JLabel();
+		btnClose.setIcon(icon);
 		btnClose.addMouseListener(new MouseListener() {
 
 			@Override
