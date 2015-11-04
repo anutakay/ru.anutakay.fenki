6b7fce0a48246027bae352a3773c7dcec2e649eb
Anya Kaygorodova <anutakay@gmail.com>
2015-11-04 14:29:30 +0600

    Рефакторинг Size и SizeTest.

--- ru.anutakay.fenki/src/ru/anutakay/fenki/model/Size.java
+++ ru.anutakay.fenki/src/ru/anutakay/fenki/model/Size.java
@@ -1,84 +1,81 @@
 package ru.anutakay.fenki.model;
 
 public class Size {
-	
-	private static final int MIN_NUM_OF_COLUMN = 1;
 
-	private static final int MIN_NUM_OF_THREAD = 2;
+    private static final int MIN_NUM_OF_COLUMN = 1;
 
-	private boolean firstCrossIsNode = true;
-	
-	private int numOfThreads;
-	
-	private int numOfColumns;
-	
-	public Size() {
-		this(MIN_NUM_OF_THREAD, MIN_NUM_OF_COLUMN);
-	}
-	
-	public Size(final int numberOfThreads, 
-			final int numberOfColumns, 
-			final boolean firstCrossIsNode) {
-		this(numberOfThreads, numberOfColumns);
-		this.firstCrossIsNode = firstCrossIsNode;
-	}
-	
-	public Size(final int numberOfThreads, 
-			final int numberOfColumns) {
-		this.numOfThreads = numberOfThreads;
-		this.numOfColumns = numberOfColumns;
+    private static final int MIN_NUM_OF_THREAD = 2;
 
-		checkNums();	
-	}
+    private boolean firstIsNode = true;
 
-	private void checkNums() {
-		if (this.numOfThreads < MIN_NUM_OF_THREAD) {
-			this.numOfThreads = MIN_NUM_OF_THREAD;
-		}
-		if (this.numOfColumns < MIN_NUM_OF_COLUMN) {
-			this.numOfColumns = MIN_NUM_OF_COLUMN;
-		}
-	}
+    private int numOfThreads;
 
-	public int getThreadNumber() {
-		return numOfThreads;
-	}
+    private int numOfColumns;
 
-	public int getColumnNumber() {
-		return numOfColumns;
-	}
+    public Size() {
+        this(MIN_NUM_OF_THREAD, MIN_NUM_OF_COLUMN);
+    }
 
-	public boolean firstCrossIsNode() {
-		return firstCrossIsNode;
-	}
+    public Size(final int numberOfThreads, final int numberOfColumns,
+            final boolean firstCrossIsNode) {
+        this(numberOfThreads, numberOfColumns);
+        this.firstIsNode = firstCrossIsNode;
+    }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + (firstCrossIsNode ? 1231 : 1237);
-		result = prime * result + numOfColumns;
-		result = prime * result + numOfThreads;
-		return result;
-	}
+    public Size(final int numberOfThreads, final int numberOfColumns) {
+        this.numOfThreads = numberOfThreads;
+        this.numOfColumns = numberOfColumns;
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Size other = (Size) obj;
-		if (firstCrossIsNode != other.firstCrossIsNode)
-			return false;
-		if (numOfColumns != other.numOfColumns)
-			return false;
-		if (numOfThreads != other.numOfThreads)
-			return false;
-		return true;
-	}
-	
-	
+        checkNums();
+    }
+
+    private void checkNums() {
+        if (numOfThreads < MIN_NUM_OF_THREAD) {
+            numOfThreads = MIN_NUM_OF_THREAD;
+        }
+        if (numOfColumns < MIN_NUM_OF_COLUMN) {
+            numOfColumns = MIN_NUM_OF_COLUMN;
+        }
+    }
+
+    public int getThreadNumber() {
+        return numOfThreads;
+    }
+
+    public int getColumnNumber() {
+        return numOfColumns;
+    }
+
+    public boolean firstCrossIsNode() {
+        return firstIsNode;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (firstIsNode ? 1231 : 1237);
+        result = prime * result + numOfColumns;
+        result = prime * result + numOfThreads;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Size other = (Size) obj;
+        if (firstIsNode != other.firstIsNode)
+            return false;
+        if (numOfColumns != other.numOfColumns)
+            return false;
+        if (numOfThreads != other.numOfThreads)
+            return false;
+        return true;
+    }
+
 }
---
+++ ru.anutakay.fenki/tests/ru/anutakay/fenki/model/SizeTest.java
@@ -0,0 +1,45 @@
+package ru.anutakay.fenki.model;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class SizeTest {
+
+    @Test
+    public void constructorTest() {
+        final Size size = new Size();
+        testSizeEquals(size, 2, 1, true);
+    }
+
+    @Test
+    public void constructorNegativeTest() {
+        final Size size = new Size(-9, 0);
+        testSizeEquals(size, 2, 1, true);
+    }
+
+    @Test
+    public void constructorFullTest() {
+        final Size size = new Size(10, 11, false);
+        testSizeEquals(size, 10, 11, false);
+    }
+
+    private void testSizeEquals(final Size size, int thread, int column,
+            boolean first) {
+        int actualThreadNum = size.getThreadNumber();
+        int actualColumnNum = size.getColumnNumber();
+        boolean actualFirstNode = size.firstCrossIsNode();
+
+        assertEquals(thread, actualThreadNum);
+        assertEquals(column, actualColumnNum);
+        assertEquals(first, actualFirstNode);
+    }
+
+    @Test
+    public void equalsTest() {
+        final Size size1 = new Size(10, 11, false);
+        final Size size2 = new Size(10, 11, false);
+        assertEquals(size1, size2);
+        assertEquals(size1.hashCode(), size2.hashCode());
+    }
+}
