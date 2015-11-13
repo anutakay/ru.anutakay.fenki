40838546840337a35e732c63a9d81662e1ee9f50
Anya Kaygorodova <anutakay@gmail.com>
2015-11-13 20:42:24 +0600

    Тест для Fragments.

--- ru.anutakay.fenki/tests/ru/anutakay/fenki/model/FragmentsTest.java
+++ ru.anutakay.fenki/tests/ru/anutakay/fenki/model/FragmentsTest.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import ru.anutakay.fenki.model.size.Size;
+import ru.anutakay.fenki.model.thread.Thread;
+import ru.anutakay.fenki.model.thread.ThreadFactory;
 
 public class FragmentsTest {
     
@@ -19,6 +21,28 @@ public class FragmentsTest {
         Size size = new Size(5, 6);
         Fragments fragments = new Fragments(size);
         assertNotNull(fragments);
+    }
+    
+    @Test 
+    public void get_empty_test() {
+        Size size = new Size(7, 2);
+        Fragments fragments = new Fragments(size);
+        ThreadIndex index = new ThreadIndex(1, 1);
+        Thread actual = fragments.getThread(index);
+        Thread expected = ThreadFactory.createEmptyThread();
+        assertEquals(expected, actual);
+    }
+    
+    @Test 
+    public void set_get_test() {
+        Size size = new Size(7, 2);
+        Fragments fragments = new Fragments(size);
+        ThreadIndex index = new ThreadIndex(1, 1);
+        Thread thread = new ThreadFactory().createThread();
+        fragments.setThread(index, thread);
+        Thread actual = fragments.getThread(index);
+        Thread expected = thread;
+        assertEquals(expected, actual);
     }
 
 }
